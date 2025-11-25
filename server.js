2e43603900809e9f934c676e574781b8b33d971a
utsavg05 <utsav.gp1204@gmail.com>
2025-11-25 22:22:33 +0530

    speeding up api response time by using cache(redis)

---
+++ server.js
@@ -0,0 +1,19 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const client = require('./client')
+
+const app = express();
+
+app.get('/', async (req, res) => {
+
+    const cacheValue = await client.get('todos');
+    if (cacheValue) return res.json(JSON.parse(cacheValue));
+
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    
+    await client.set('todos', JSON.stringify(data));
+    await client.expire('todos', 30);
+    return res.json(data);
+})
+
+app.listen(9000);
